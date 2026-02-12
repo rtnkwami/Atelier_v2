435bf8af9ee89f7a4eecc71922951a3404424436
rtnkwami <nukukwami@outlook.com>
2026-02-12 16:18:15 +0000

    feat(inventory): add rpc request validation pipe

--- apps/inventory/src/pipes/request.validation.pipe.ts
+++ apps/inventory/src/pipes/request.validation.pipe.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import z, { ZodType } from 'zod';
 
 export class HttpRequestValidationPipe implements PipeTransform {
@@ -14,6 +15,24 @@ export class HttpRequestValidationPipe implements PipeTransform {
         message: 'request body is invalid',
         invalidFields: flattenedErrors.fieldErrors,
         unknownFields: flattenedErrors.formErrors,
+      });
+    }
+    return result.data;
+  }
+}
+
+export class RpcRequestValidationPipe implements PipeTransform {
+  constructor(private readonly schema: ZodType) {}
+
+  transform(value: any) {
+    const result = this.schema.safeParse(value);
+    if (result.error) {
+      const flattenedErrors = z.flattenError(result.error);
+
+      throw new RpcException({
+        message: 'request payload does not match expected schema',
+        invalidFields: flattenedErrors.fieldErrors,
+        unknownField: flattenedErrors.formErrors,
       });
     }
     return result.data;
