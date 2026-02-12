98eab70c253acb55342f4a3f99924578c45f2c14
rtnkwami <nukukwami@outlook.com>
2026-02-12 11:07:42 +0000

    feat(inventory): add product creation function

--- apps/inventory/src/app.http.controller.ts
+++ apps/inventory/src/app.http.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
 import { InventoryService } from './app.service';
+import {
+  CreateProductSchema,
+  type ProductCreate,
+} from 'src/validation/product.validation';
+import { HttpRequestValidationPipe } from './pipes/request.validation.pipe';
+import { Product } from './entities/product.entity';
 
 @Controller()
 export class HttpController {
   constructor(private readonly inventoryService: InventoryService) {}
+
+  @Post()
+  @UsePipes(new HttpRequestValidationPipe(CreateProductSchema))
+  private createProduct(@Body() data: ProductCreate): Promise<Product> {
+    return this.inventoryService.createProduct(data);
+  }
 
   @Get()
   getHello(): string {
--- apps/inventory/src/app.service.ts
+++ apps/inventory/src/app.service.ts
@@ -1,7 +1,21 @@
+import { EntityManager, MikroORM } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
+import { Product } from './entities/product.entity';
+import { ProductCreate } from 'src/validation/product.validation';
 
 @Injectable()
 export class InventoryService {
+  constructor(
+    private readonly orm: MikroORM,
+    private readonly em: EntityManager,
+  ) {}
+
+  public async createProduct(data: ProductCreate) {
+    const product = this.em.create(Product, data);
+    await this.em.flush();
+    return product;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
